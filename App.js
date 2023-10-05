f957d4ceb0ec83ac61b4e7d84f2eea1e4c6bfdab
Lakshmi Panguluri <lp755@iris.nau.edu>
2023-10-05 11:26:42 -0700

    e2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1",{id:"heading1", abc: "xyz"},"Hello World from React");
 const parent1 = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{id:"heading"},"h1 tag nested")));
 
